db06166d5b296e377062fbbd66bd5924b776f536
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-10-13 23:24:09 -0700

    Created job constructor.  DPS is currently only child as all healers are ranged and all tanks are melee.

--- javascript/script.js
+++ javascript/script.js
@@ -7,37 +7,34 @@ $.carousel('pause').carousel({
 
 
 // Name is the name of the job
-// Type is support, DPS, or tanky
-// Style is ranged or melee 
+// Role is support, DPS, or tanky
+// Focus is what class is good at. Shield, multi-target, etc.
 
-class DPS {
-    constructor(name, type, style) {
+class Job {
+    constructor(name, role, focus){
         this.name = name,
-        this.type = type,
-        this.style = style
-    }
-    /*see(){
-        console.log(`The class is ${this.name} and is a ${this.type} and plays with ${this.style}.`)
-    }*/
-};
-
-class Tank {
-    constructor(name, type, style) {
-        this.name = name,
-        this.type = type,
-        this.style = style
+        this.role = role,
+        this.focus = focus
     }
 };
 
-class Healer {
-    constructor(name, type, style) {
-        this.name = name,
-        this.type = type,
-        this.style = style
+//
+
+class DPS extends Job {
+    constructor(name, role, focus, caster, ranged){
+        super(name, role, focus);
+        this.caster = true,
+        this.ranged = true;
+
     }
-};
+}
 
 
-const redMage = new DPS("Red Made", "Support", "Ranged");
-const paladin = new Tank("Paladin", "Support", "Melee")
+//DPS variables
+const redMage = new DPS("Red Made", "Support", "Ranged", "Ranged");
+
+//Tank variables
+const paladin = new Job("Paladin", "Tank", "Support")
+
+//Healer variables
 
