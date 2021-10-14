4b74f4d942934bd01b7eeddc0ce8e3a505e65fe9
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-10-13 22:09:41 -0700

    Added tank class and paladin class constructor.

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,35 @@
-/* function message() {
+function message() {
     alert("Welcome to Kupop!  This site is currently underconstruction, but please feel free to have a look around!");
 }
 $.carousel('pause').carousel({
 
 });
-*/
 
- 
+
+// Name is the name of the job
+// Type is support, DPS, or tanky
+// Style is ranged or melee 
+
 class DPS {
     constructor(name, type, style) {
-        this.name = name;
-        this.type = type
+        this.name = name,
+        this.type = type,
         this.style = style
     }
-    see(){
+    /*see(){
         console.log(`The class is ${this.name} and is a ${this.type} and plays with ${this.style}.`)
-    }
+    }*/
 };
 
+class Tank {
+    constructor(name, type, style) {
+        this.name = name,
+        this.type = type,
+        this.style = style
+    }
+}
+
+
 const redMage = new DPS("Red Made", "Support", "Ranged");
+const paladin = new Tank("Paladin", "Support", "Ranged")
+
